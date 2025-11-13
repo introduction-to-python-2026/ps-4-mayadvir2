0f54e364b312f17cbf902854c2f745e799c2328f
mayadvir2 <mayadvir222@gmail.com>
2025-11-13 17:14:44 +0200

    maya 1

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,26 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
-
+  lst = []
+  i=0
+  while i < len(formula):
+    str = formula[i]
+    i+=1
+    while i < len(formula) and not formula[i].isupper():
+      str += formula[i]
+      i+=1
+    lst.append(str)
+  return lst
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+  string = ""
+  num = 1
+  index = 0
+  for i in range(len(formula)):
+    if not formula[i].isdigit():
+        string += formula[i]
+        index += 1
+    else:
+        break
+  if index < len(formula):
+      num = int(formula[index:])
+  return (string , num)
+
